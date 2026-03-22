5a0748d70372fde5032310b3cd2191729393e39f
PeaceMelodi <peacemelodi1@gmail.com>
2026-03-22 18:26:51 +0100

    Fix scheduler save and mailer IPv4

--- frontend/src/app/links/layout.js
+++ frontend/src/app/links/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 lg:ml-60 pt-16 lg:pt-4 px-4 lg:px-8 py-8 max-w-5xl">
+      <main className="flex-1 lg:ml-60 pt-16 lg:pt-0 px-4 lg:px-8 py-8 overflow-x-hidden" style={{ maxWidth: '100vw', minWidth: 0 }}>
         {children}
       </main>
     </div>
